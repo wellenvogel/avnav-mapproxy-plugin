c9180fd6620f860d729d5276fb6a921e282bdb9b
andreas <andreas@wellenvogel.de>
2021-02-03 20:34:14 +0100

    download boxes and include in package

--- buildPkg.sh
+++ buildPkg.sh
@@ -1,13 +1,38 @@
 #! /bin/sh
 #package build using https://github.com/goreleaser/nfpm on docker
+err(){
+  echo "ERROR: $*"
+  exit 1
+}
 pdir=`dirname $0`
 pdir=`readlink -f "$pdir"`
+boxesRepo='https://raw.githubusercontent.com/free-x/mbtiles-nautical-boxes/contrib/iho/contrib/iho'
+boxes="allcountries.bbox"
+buildDir="$pdir/build/boxes"
 #set -x
 config=package.yaml
+force=0
+if [ "$1" = "-f" ] ; then
+  force=1
+  shift
+fi  
 version="$1"
 if [ "$version" = "" ] ; then
   version=`date '+%Y%m%d'`
 fi
+echo loading/checking boxes
+if [ ! -d "$buildDir" ]; then
+  mkdir -p "$buildDir" || err unable to create $buildDir
+fi
+for box in `echo $boxes`
+do
+  bf="$buildDir/$box"
+  if [ ! -f "$bf" -o $force = 1 ]; then
+    url="$boxesRepo/$box"
+    echo downloading "$url"
+    curl -o "$bf" -f "$url" || err unable to download "$url"
+  fi
+done
 echo building version $version
 tmpf=package$$.yaml
 rm -f $tmpf
